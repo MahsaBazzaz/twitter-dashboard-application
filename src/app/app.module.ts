d2f596da24579b9ca0aa1e14fa2d33084f8dbfc3
popsiqle <popsiqle@hotmail.com>
2022-04-19 10:11:27 +0430

    change first chart to timeseries

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { DefaultModule } from './layouts/default/default.module';
+import { WordcloudComponent } from './wordcloud/wordcloud.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    WordcloudComponent,
   ],
   imports: [
     BrowserModule,
--- src/app/shared/widgets/area/area.component.ts
+++ src/app/shared/widgets/area/area.component.ts
@@ -20,27 +20,58 @@ export class AreaComponent implements OnInit {
   ngOnInit() {
     this.chartOptions = {
       chart: {
-        type: 'area'
-      },
-      title: {
-        text: 'Random DATA'
-      },
-      subtitle: {
-        text: 'Demo'
-      },
-      tooltip: {
-        split: true,
-        valueSuffix: ' millions'
-      },
-      credits: {
+        zoomType: 'x'
+    },
+    title: {
+        text: 'Tweets rate over time of day'
+    },
+    subtitle: {
+        text: document.ontouchstart === undefined ?
+            'Click and drag in the plot area to zoom in' : 'Pinch the chart to zoom in'
+    },
+    xAxis: {
+        type: 'time'
+    },
+    yAxis: {
+        title: {
+            text: 'Number of Tweets'
+        }
+    },
+    legend: {
         enabled: false
-      },
-      exporting: {
-        enabled: true,
-      },
-      series: this.data
+    },
+    plotOptions: {
+        area: {
+            fillColor: {
+                linearGradient: {
+                    x1: 0,
+                    y1: 0,
+                    x2: 0,
+                    y2: 1
+                },
+                stops: [
+                    [0, Highcharts.getOptions().colors[0]],
+                    [1, Highcharts.color(Highcharts.getOptions().colors[0]).setOpacity(0).get('rgba')]
+                ]
+            },
+            marker: {
+                radius: 2
+            },
+            lineWidth: 1,
+            states: {
+                hover: {
+                    lineWidth: 1
+                }
+            },
+            threshold: null
+        }
+    },
+      series: [{
+        type: 'area',
+        name: 'Tweets Rate',
+        data: this.data
+    }]
     };
-
     HC_exporting(Highcharts);
 
     setTimeout(() => {
