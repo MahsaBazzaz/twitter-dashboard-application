565aa533dc3fece01dcfb3d5d608ad49ddcc0abb
popsiqle <popsiqle@hotmail.com>
2022-04-20 11:26:35 +0430

    clean to update

--- src/app/modules/accounts/accounts.component.ts
+++ src/app/modules/accounts/accounts.component.ts
@@ -1,25 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { ResponseSchema, User } from 'src/dtos';
 import { AccountsService } from './accounts.service';
 
 @Component({
   selector: 'app-accounts',
   templateUrl: './accounts.component.html',
+  // template: `<div #container></div>`,
   styleUrls: ['./accounts.component.scss']
 })
 export class AccountsComponent implements OnInit {
 
+  // @ViewChild("tref", {read: ElementRef}) tref: ElementRef;
   name = "username";
   constructor(private accountsService: AccountsService) { }
 
   ngOnInit() {
-    this.getAllUsers();
+    // this.getAllUsers();
   }
 
   getAllUsers() {
     this.accountsService.getAllUsers()
       .subscribe((data: ResponseSchema<User[]>) => {
         console.log(data);
+        // <app-useraccount [username]="" [detail]=""></app-useraccount>
       });
   }
 }
--- src/app/shared/components/useraccount/useraccount.component.ts
+++ src/app/shared/components/useraccount/useraccount.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-useraccount',
@@ -8,8 +8,8 @@ import { Component, OnInit } from '@angular/core';
 export class UseraccountComponent implements OnInit {
 
   constructor() { }
-  username : string = "mahsabzz";
-  detail : string = "##";
+  @Input() username : string = "mahsabzz";
+  @Input() detail : string = "##";
   ngOnInit() {
   }
 
