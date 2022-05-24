8e7b74e5c9c9171604e5284a94119e8bad3fe5e7
popsiqle <popsiqle@hotmail.com>
2022-05-24 12:26:28 +0430

    fix the bug with pagination

--- src/app/shared/components/my-custom-paginator-intl/my-custom-paginator-intl.component.ts
+++ src/app/shared/components/my-custom-paginator-intl/my-custom-paginator-intl.component.ts
@@ -1,5 +1,5 @@
-import {Component, EventEmitter, Output} from '@angular/core';
-import {MatPaginatorIntl, MatPaginatorModule, PageEvent} from '@angular/material/paginator';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { MatPaginatorIntl, MatPaginatorModule, PageEvent } from '@angular/material/paginator';
 import { PaginatorService } from './paginator.service';
 
 @Component({
@@ -7,21 +7,35 @@ import { PaginatorService } from './paginator.service';
   templateUrl: './my-custom-paginator-intl.component.html',
   styleUrls: ['./my-custom-paginator-intl.component.scss']
 })
-export class MyCustomPaginatorIntlComponent  {
+export class MyCustomPaginatorIntlComponent {
 
   length = 500;
   pageSize = 10;
   pageIndex = 0;
   pageSizeOptions = [5, 10, 25];
-  showFirstLastButtons = true;
+  showFirstLastButtons = false;
 
-  constructor( private service : PaginatorService) {}
+  constructor(private service: PaginatorService) { }
+  ngAfterViewInit() {
 
+    this.getTweetCount();
+    setInterval(() => {
+      this.getTweetCount();
+    }, 30000);
+  }
   handlePageEvent(event: PageEvent) {
-    this.length = event.length;
+    this.getTweetCount();
     this.pageSize = event.pageSize;
     this.pageIndex = event.pageIndex;
-    this.service.emit(this.pageIndex , this.pageSize)
+    this.service.emit(this.pageIndex, this.pageSize)
+  }
+
+  getTweetCount() {
+    this.service.getTweetCount().subscribe(res => {
+      if (res.ok) {
+        this.length = Math.ceil(res.ok.data / this.pageSize)
+      }
+    });
   }
 
 }
--- src/app/shared/components/my-custom-paginator-intl/paginator.service.ts
+++ src/app/shared/components/my-custom-paginator-intl/paginator.service.ts
@@ -5,18 +5,22 @@ import { catchError, retry, tap } from 'rxjs/operators';
 import { ResponseSchema, Tweet, TweetWithImage, User } from 'src/dtos';
 
 @Injectable({
-    providedIn: 'root'
+  providedIn: 'root'
 })
 export class PaginatorService {
+  constructor(private http: HttpClient) { }
 
   @Output() aClickedEvent = new EventEmitter<PaginatorDto>();
-   
-  emit(pageIndex, pageSize){
-    this.aClickedEvent.emit({index : pageIndex, size : pageSize});
+  emit(pageIndex, pageSize) {
+    this.aClickedEvent.emit({ index: pageIndex, size: pageSize });
+  }
+
+  getTweetCount(): Observable<ResponseSchema<number>> {
+    return this.http.get<ResponseSchema<number>>("http://127.0.0.1:3000/getTweetsCount");
   }
 }
 
-export interface PaginatorDto{
-    index : number,
-    size : number
+export interface PaginatorDto {
+  index: number,
+  size: number
 }
