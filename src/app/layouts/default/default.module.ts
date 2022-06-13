2a8dcdb2c5c47f007571008eeec2b70c8cddf358
popsiqle <popsiqle@hotmail.com>
2022-06-13 14:37:06 +0430

    tweak

--- src/app/layouts/default/default.module.ts
+++ src/app/layouts/default/default.module.ts
@@ -17,6 +17,13 @@ import { FlexLayoutModule } from '@angular/flex-layout';
 import { DashboardService } from 'src/app/modules/dashboard/dashboard.service';
 import { AccountsComponent } from 'src/app/modules/accounts/accounts.component';
 import { KeywordsComponent } from 'src/app/modules/keywords/keywords.component';
+import { WordcloudComponent } from 'src/app/shared/widgets/wordcloud/wordcloud.component';
+import { AreaComponent } from 'src/app/shared/widgets/area/area.component';
+import { TopUsersComponent } from 'src/app/shared/components/top-users/top-users.component';
+import { TopKeywordsComponent } from 'src/app/shared/components/top-keywords/top-keywords.component';
+import { TopTweetsComponent } from 'src/app/shared/components/top-tweets/top-tweets.component';
+import { PieComponent } from 'src/app/shared/widgets/pie/pie.component';
+import { GraphComponent } from 'src/app/shared/widgets/graph/graph.component';
 
 @NgModule({
   declarations: [
@@ -25,6 +32,15 @@ import { KeywordsComponent } from 'src/app/modules/keywords/keywords.component';
     PostsComponent,
     AccountsComponent,
     KeywordsComponent
+  ],
+  entryComponents: [
+    WordcloudComponent,
+    AreaComponent,
+    TopUsersComponent,
+    TopKeywordsComponent,
+    TopTweetsComponent,
+    PieComponent,
+    GraphComponent
   ],
   imports: [
     CommonModule,
